87e4c4aaf32f4d53b9d649ed88f3d384f7f57779
Om Rishi Kumar <mahtopradeep802@gmail.com>
2025-07-26 07:43:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.ibb.co',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
