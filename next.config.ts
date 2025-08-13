decdf292b7f9adcc82adac78dac0ae059c61947a
Om Rishi Kumar <mahtopradeep802@gmail.com>
2025-08-13 11:25:33 +0000

    Bro what is this I got this in my mail

--- next.config.ts
+++ next.config.ts
@@ -43,6 +43,9 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       }
     ],
+    // This is the key change to reduce image transformations
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
 };
 
