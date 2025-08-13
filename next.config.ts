eab9b19952ef643fa2864b5523cc33531ff159f3
Om Rishi Kumar <mahtopradeep802@gmail.com>
2025-08-13 13:04:38 +0000

    {   "type": "service_account",   "project_id": "tunestream-m2c5m",   "pr

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 
 import type {NextConfig} from 'next';
-import withPWA from '@ducanh2912/next-pwa';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -49,12 +48,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-const pwaConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
-
-export default pwaConfig(nextConfig);
+export default nextConfig;
