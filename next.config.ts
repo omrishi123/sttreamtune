b0ac01856caa05d58396d30916c644599ad174cf
Om Rishi Kumar <mahtopradeep802@gmail.com>
2025-12-06 05:27:45 +0000

    DO THIS Nice, this is your StreamTune repo 😎 that helps.
    Short answer:

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '100mb',
+    },
+  },
   reactStrictMode: false,
   typescript: {
     ignoreBuildErrors: true,
